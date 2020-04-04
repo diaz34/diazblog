14b820d8692fe4885805d6cdecd9c19ac444205a
diaz34 <63161685+diaz34@users.noreply.github.com>
2020-04-04 20:03:23 +0300

    dsf

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<link rel="shortcut icon" href="/images/faviconnew.ico" />
+
+<meta charset="utf-8">
+<meta name="description" content="" />
+
+<title>diaz-blog.ru</title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="css/reset.css" />
+
+
+</head>
+
+<body class="giantdesign">
+    <!-- [START] Kenburn Slideshow -->
+    <div class="kenburns-slideshow">
+        <ul class="slideshow large-12 medium-12 small-12 columns">
+            <li><figure></figure></li>
+            <li><figure></figure></li>
+            <li><figure></figure></li>
+            <li><figure></figure></li>
+            <li><figure></figure></li>
+            <li><figure></figure></li>
+        </ul>
+    </div>
+    <!-- [END] Kenburn Slideshow -->
+
+    <!--HEADER-->
+        <header>
+
+            <div id="top-name"><a href="/">#Diaz</a></div>
+            <div id="top-nav">
+                
+                <ul>
+                    
+                    <li><a href="/"Главная</a></li>
+                    
+                    <li><a href="/contacts">Контакты</a></li>
+                    
+                    <li><a href="#">Сделать заказ</a></li>
+                    
+                    <li><a href="/portfolio">Портфолио</a></li>
+                    
+                    <li><a href="/recalls">Отзывы</a></li>
+
+                    <li><a href="/garantii">Гарантии</a></li>
+
+                </ul>
+
+            </div>
+
+            <div class="cl-f"></div>
+
+        </header>
+        <!--END HEADER-->
+
+<div style="height:auto; min-height:100%; ">     
+	<div style="text-align: center; width:800px; margin-left: -400px; position:absolute; top: 30%; left:50%;">
+        <h1 style="margin:0; font-size:60px; line-height:150px; font-weight:bold;">diaz-blog.ru</h1>
+      	<h2 style="font-size: 30px;">Site on reconstruction</h2>
+    </div>
+<body>
